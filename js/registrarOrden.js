bfc470ce8ddcc790620389b6aecbff8ec60e42ac
Alerzi88 <alexisht1998@gmail.com>
2018-12-13 21:24:07 -0300

    cambios

--- js/registrarOrden.js
+++ js/registrarOrden.js
@@ -11,8 +11,9 @@ formulario.addEventListener('submit', function(e){
     })
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             Push.create("Nueva Orden de Trabajo",{
-                body: "Se creo una nueva orden, Si quieres verla inicia session",
+                body: "Se creo una nueva orden, Si quieres verla ve a ´Ordenes´",
                 icon: "img/icono_192x192.jpg",
                 timeout: 4000,
                 onClick: function(){                  
